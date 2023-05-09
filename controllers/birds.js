553e7d64742371eb6c05aedb6894ebf90b325885
naenha <daon318@gmail.com>
2023-05-09 17:03:51 +0100

    db working

---
+++ controllers/birds.js
@@ -0,0 +1,30 @@
+var bodyParser = require("body-parser");
+var req = require('request');
+var Bird = require('../models/birds');
+var path = require('path');
+
+
+exports.create = function (req, res) {
+
+    var userData = req.body;
+
+    var bird = new Bird({
+        userName: userData.userName,
+        observedAt: userData.observedAt,
+        description: userData.description,
+        img: req.file.path
+    });
+
+    bird.save(function (err, results) {
+        if (err)
+            res.status(500).send('Invalid data!');
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(bird));
+    });
+};
+
+
+
+
+
+
---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser= require("body-parser");
+var bird = require('../controllers/birds');
+var multer = require('multer');
+
+// storage defines the storage options to be used for file upload with multer
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/');
+  },
+  filename: function (req, file, cb) {
+    var original = file.originalname;
+    var file_extension = original.split(".");
+    // Make the file name the date + the file extension
+    filename =  Date.now() + '.' + file_extension[file_extension.length-1];
+    cb(null, filename);
+  }
+});
+var upload = multer({ storage: storage });
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'My Form' });
+});
+
+
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'My Form' });
+});
+
+router.get('/add', function(req, res, next) {
+  res.render('add', { title: 'Add a new Character to the DB' });
+});
+
+router.post('/add', upload.single('myImg'), function(req, res) {
+  console.log(req);
+  bird.create(req,res);
+});
+
+
+
+
+//router.post('/index', character.getAge);
+
+module.exports = router;
