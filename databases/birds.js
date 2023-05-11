74e90c05daeb1b03d2f7af60d1cf10adb5fa4235
Haoming Liu <hliu90@sheffield.ac.uk>
2023-05-11 13:52:02 +0100

    add mongoose connection option

--- databases/birds.js
+++ databases/birds.js
@@ -6,7 +6,7 @@ var mongoDB = 'mongodb://localhost:27017/birds';
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(mongoDB);
+mongoose.connect(mongoDB, { userNewUrlParser: true, useUnifiedTopology: true, checkServerIdentity: false });
 var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
