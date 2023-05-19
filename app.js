e420ac2addecf4cf61c360b6b6230be44be69113
acb20dx <1499949782@qq.com>
2023-05-19 15:38:15 +0100

    css

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use('/public/', express.static('./public/'));
 
 
 app.use('/', index);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
